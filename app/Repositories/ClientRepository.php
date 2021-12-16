04fd93bc5d5b752da15b97f14f283d3d35757398
Desy2409 <desire.tchipotou@gmail.com>
2021-12-16 13:02:40 +0000

    User type - Page operation

--- app/Repositories/ClientRepository.php
+++ app/Repositories/ClientRepository.php
@@ -7,12 +7,43 @@ use App\Repositories\Repository;
 
 class ClientRepository extends Repository
 {
-    public function clientReport($code = false, $reference = false, $lastName = false, $firstName = false, $socialReason = false, $rccmNumber = false, $ccNumber = false, $personType = false, $startDate = null, $endDate = null)
+    public function clientReport($code = false, $reference = false, $lastName = false, $firstName = false, $socialReason = false, $rccmNumber = false, $ccNumber = false, $personType = false, $settings = false, $startDate = null, $endDate = null)
     {
-        if (!$code && !$reference && !$lastName && !$firstName && !$socialReason && !$rccmNumber && !$ccNumber && !$personType && $startDate == null && $endDate == null) {
-            $clients=null;
+        if (!$code && !$reference && !$lastName && !$firstName && !$socialReason && !$rccmNumber && !$ccNumber && !$personType &&!$settings && $startDate == null && $endDate == null) {
+            $clients = null;
         } else {
             $clients = Client::with('person');
+            if ($code) {
+                array_push($this->columns, 'code');
+            }
+            if ($reference) {
+                array_push($this->columns, 'reference');
+            }
+            if ($lastName) {
+                array_push($this->columns, 'last_name');
+            }
+            if ($firstName) {
+                array_push($this->columns, 'first_name');
+            }
+            if ($socialReason) {
+                array_push($this->columns, 'social_reason');
+            }
+            if ($rccmNumber) {
+                array_push($this->columns, 'rccm_number');
+            }
+            if ($ccNumber) {
+                array_push($this->columns, 'cc_number');
+            }
+            if ($personType) {
+                array_push($this->columns, 'person_type');
+            }
+            if ($settings) {
+                array_push($this->columns, 'settings');
+            }
+            if ($startDate && $endDate) {
+                $clients->whereBetween('created_at', [$startDate, $endDate]);
+            }
+            $clients = $clients->get($this->columns);
         }
 
         return $clients;
