259236260018af14e2aa5903b4553b3c5826cfc9
Desy2409 <desire.tchipotou@gmail.com>
2022-01-10 15:58:41 +0000

    RemovalOrder onCarrierSelect

--- app/Models/ProductSale.php
+++ app/Models/ProductSale.php
@@ -7,10 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductSale extends Model
 {
-    protected $fillable = [
-        'quantity',
-        'unit_price',
-    ];
+    protected $appends = ['quantity_to_deliver', 'delivered_quantity', 'remaining_quantity'];
 
     public function product()
     {
@@ -25,5 +22,23 @@ class ProductSale extends Model
     public function unity()
     {
         return $this->belongsTo(Unity::class);
+    }
+
+    public function getQuantityToDeliverAttribute()
+    {
+        return ProductSale::where('sale_id', $this->sale->id)->where('product_id', '=', $this->product->id)->first()->quantity;
+    }
+
+    public function getDeliveredQuantityAttribute()
+    {
+        $deliveredQuantity = 0;
+        $deliveredQuantity = ProductDeliveryNote::join('client_delivery_notes', 'client_delivery_notes.id', '=', 'product_client_delivery_notes.delivery_note_id')
+            ->join('sales', 'sales.id', '=', 'client_delivery_notes.sale_id')->where('sales.id', $this->sale->id)->where('product_id', '=', $this->product->id)->sum('quantity');
+        return $deliveredQuantity;
+    }
+
+    public function getRemainingQuantityAttribute()
+    {
+        return ($this->getQuantityToDeliverAttribute() > $this->getDeliveredQuantityAttribute()) ? ($this->getQuantityToDeliverAttribute() - $this->getDeliveredQuantityAttribute()) : 0;
     }
 }
